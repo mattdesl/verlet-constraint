93d61140be34bc3f96ec7697a5ae9a02fb03b36f
mattdesl <dave.des@gmail.com>
2014-11-15 17:01:28 -0500

    using modular build

--- 2d.js
+++ 2d.js
@@ -1,2 +1,9 @@
-var vec = require('gl-matrix').vec2
-module.exports = require('./lib/build')(vec)
+var vec2 = {
+    create: require('gl-vec2/create'),
+    add: require('gl-vec2/add'),
+    dot: require('gl-vec2/dot'),
+    sub: require('gl-vec2/subtract'),
+    scale: require('gl-vec2/scale'),
+    distance: require('gl-vec2/distance')
+}
+module.exports = require('./lib/build')(vec2)
--- 3d.js
+++ 3d.js
@@ -1,2 +1,9 @@
-var vec = require('gl-matrix').vec3
-module.exports = require('./lib/build')(vec)
+var vec3 = {
+    create: require('gl-vec3/create'),
+    add: require('gl-vec3/add'),
+    dot: require('gl-vec3/dot'),
+    sub: require('gl-vec3/subtract'),
+    scale: require('gl-vec3/scale'),
+    distance: require('gl-vec3/distance')
+}
+module.exports = require('./lib/build')(vec3)
